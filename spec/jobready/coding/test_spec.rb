cf9b2d1c09c93061109ef041994f5a6ba8c8a377
Nigel Williams <nigelw@vurt.net>
2020-04-26 13:20:34 +1000

    Remove placeholder failing test

--- spec/jobready/coding/test_spec.rb
+++ spec/jobready/coding/test_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Jobready::Coding::Test do
   it "has a version number" do
     expect(Jobready::Coding::Test::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
